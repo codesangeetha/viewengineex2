6b06140e1ef493b924ef181af65bf4fd4117988d
Sreekumar MC <sreekumar.mc72@gmail.com>
2024-08-13 00:30:13 +0530

    hbs files added

--- app.js
+++ app.js
@@ -14,6 +14,19 @@ app.get('/hbsdemo1', (req, res) => {
 
 });
 
+app.get('/tabledemo', (req, res) => {
+    const students = [
+        { studentName: 'John Doe', mark: 85 },
+        { studentName: 'Jane Smith', mark: 92 },
+        { studentName: 'Bob Johnson', mark: 78 },
+        { studentName: 'Alice Brown', mark: 95 },
+        { studentName: 'Mike Davis', mark: 88 },
+        { studentName: 'Emily Taylor', mark: 82 }
+      ];
+    res.render('marks', { arr: students});
+
+});
+
 app.listen(4000, () => {
     console.log('server started at 4000');
 });
